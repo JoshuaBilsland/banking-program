03507023f78df7a51cfb81c6675d9e1e9c2c6651
Josh Bilsland <joshua.bilsland12@gmail.com>
2025-10-22 20:10:20 +0100

    feat: add loop and switch

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,28 @@ public class Main {
         double balance;
         boolean isRunning = true;
         int choice;
+        while(isRunning) {
+            System.out.println("***************");
+            System.out.println("BANKING PROGRAM");
+            System.out.println("***************");
+            System.out.println("1. Show Balance");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Exit");
+            System.out.println("***************");
 
-        System.out.println("***************");
-        System.out.println("BANKING PROGRAM");
-        System.out.println("***************");
-        System.out.println("1. Show Balance");
-        System.out.println("2. Deposit");
-        System.out.println("3. Withdraw");
-        System.out.println("4. Exit");
-        System.out.println("***************");
+            System.out.print("Enter your choice (1-4): ");
+            choice = scanner.nextInt();
 
-        System.out.print("Enter your choice (1-4): ");
-        choice = scanner.nextInt();
-        System.out.println(choice);
+            switch(choice){
+                case 1 -> System.out.println("SHOW BALANCE");
+                case 2 -> System.out.println("DEPOSIT");
+                case 3 -> System.out.println("WITHDRAW");
+                case 4 -> isRunning = false;
+                default -> System.out.println("INVALID CHOICE");
+            }
+        }
+
         scanner.close();
     }
 }
